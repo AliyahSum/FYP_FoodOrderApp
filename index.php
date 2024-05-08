0bcd9c935cced46ccf40f1c962e8708c925c1a58
thccthc <22011587@rp.edu.sg>
2024-05-08 11:31:30 +0800

    initial commit

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <h1>Hello</h1>
         <?php
         // put your code here
         ?>
+        <h1>Bye</h1>
+        
+        <h2>yo</h2>
+        
+        fhlkjjkl
     </body>
 </html>
